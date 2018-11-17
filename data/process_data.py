a5089f8f4ff7f45b4e0d4bbc381aace876215918
Anthony Giustino <giustino.anthony@gmail.com>
2018-11-17 15:36:29 -0500

    feat: Create functions for process_data script

--- data/process_data.py
+++ data/process_data.py
@@ -1,16 +1,83 @@
 import sys
+import numpy as np 
+import pandas as pd 
+from sqlalchemy import create_engine 
+import os 
 
 
 def load_data(messages_filepath, categories_filepath):
-    pass
+  '''Loads messages and categories datasets into program and merges datasets 
+  together
+  
+  Arguments:
+    messages_filepath {str} -- csv file path
+    categories_filepath {str} -- csv file path
+  '''
+
+  # Load datasets
+  messages = pd.read_csv(messages_filepath)
+  categories = pd.read_csv(categories_filepath)
+
+  # Merge datasets
+  return pd.merge(messages, categories, on='id')
 
 
 def clean_data(df):
-    pass
+  '''Performs cleaning steps on data
+  
+  Arguments:
+    df {dataframe} -- dataframe from load_data function
+  '''
+
+  ## 1. Split categories into separate category columns ##
+  # Create a dataframe of the 36 individual category columns
+  categories = pd.DataFrame(df['categories'].str.split(';', expand=True))
+
+  # Select the first row of the categories dataframe
+  row = categories[0:1]
+
+  # Extract a list of new column names for categories
+  category_colnames = row.apply(lambda x: x[0][:-2])
+
+  # Rename the columns of `categories`
+  categories.columns = category_colnames
+
+
+  ## 2. Convert category values to just numbers 0 or 1 ##
+  for column in categories:
+    # set each value to be the last character of the string
+    categories[column] = categories[column].str[-1]
+    
+    # convert column from string to numeric
+    categories[column] = categories[column].astype('int')
+
+
+  ## 3. Replace categories column in df with new category columns ##
+  # drop the original categories column from `df`
+  df.drop('categories', axis=1, inplace=True)
+
+
+  # concatenate the original dataframe with the new `categories` dataframe
+  df = pd.concat([df,categories], axis=1)
+
+
+  ## 4. Remove duplicates ##
+  # drop duplicates
+  df.drop_duplicates(inplace=True)
+
+  return df
+
 
 
 def save_data(df, database_filename):
-    pass  
+  '''Saves dataframe into a SQL database 
+  
+  Arguments:
+    df {dataframe} -- dataframe after being loaded and cleaned
+    database_filename {str} -- path to [database filename] with '.db' extension
+  '''
+  engine = create_engine('sqlite:///' + database_filename)
+  df.to_sql(database_filename, engine, index=False)  
 
 
 def main():
